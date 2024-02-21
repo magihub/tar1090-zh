f921ddefe2032769d23944d257cbbdf16aa7ab21
FAN <236659588@qq.com>
2024-02-21 12:45:53 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ trap 'echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
 renice 10 $$
 
 srcdir=/run/readsb
-repo="https://ghproxy.com/https://github.com/HLLF-FAN/tar1090-zh"
-db_repo="https://ghproxy.com/https://github.com/HLLF-FAN/tar1090-db"
+repo="https://gh-proxy.com/https://github.com/HLLF-FAN/tar1090-zh"
+db_repo="https://gh-proxy.com/https://github.com/HLLF-FAN/tar1090-db"
 
 # optional command line options for this install script
 # $1: data source directory
